3eb4cf59a7803dcd849acfdd85e52ad3cbca78a2
stefanosmanolas <stefanosmanolas09@outlook.com>
2026-06-09 19:44:09 +0300

    Update pingpong.py
    we put some colisions and fix some bugs

--- pingpong.py
+++ pingpong.py
@@ -10,9 +10,6 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Ping Pong")
 
 BLUE = (135, 206, 235)
-WHITE = (255, 255, 255)
-
-font = pygame.font.SysFont("Arial", 30)
 
 ball = pygame.image.load("ball.png")
 ball = pygame.transform.scale(ball, (30, 30))
@@ -57,14 +54,37 @@ while running:
     if keys[pygame.K_DOWN]:
         p2_y += speed
 
+    if p1_y < 0:
+        p1_y = 0
+    if p1_y + 120 > HEIGHT:
+        p1_y = HEIGHT - 120
+
+    if p2_y < 0:
+        p2_y = 0
+    if p2_y + 120 > HEIGHT:
+        p2_y = HEIGHT - 120
+
     ball_x += ball_dx
     ball_y += ball_dy
 
     if ball_y <= 0 or ball_y + 30 >= HEIGHT:
         ball_dy *= -1
 
-    if ball_x <= 0 or ball_x + 30 >= WIDTH:
+    ball_rect = pygame.Rect(ball_x, ball_y, 30, 30)
+    p1_rect = pygame.Rect(p1_x, p1_y, 20, 120)
+    p2_rect = pygame.Rect(p2_x, p2_y, 20, 120)
+
+    if ball_rect.colliderect(p1_rect):
         ball_dx *= -1
+        ball_x = p1_x + 20
+
+    if ball_rect.colliderect(p2_rect):
+        ball_dx *= -1
+        ball_x = p2_x - 30
+
+    if ball_x <= 0 or ball_x >= WIDTH:
+        ball_x = WIDTH // 2
+        ball_y = HEIGHT // 2
 
     screen.fill(BLUE)
 
