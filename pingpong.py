1fa9602d696f60c0c27d112e3f6e5e97cde55767
stefanosmanolas <stefanosmanolas09@outlook.com>
2026-06-09 19:28:06 +0300

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -7,9 +7,15 @@ WIDTH = 500
 HEIGHT = 700
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Το Παιχνίδι Μου")
+pygame.display.set_caption("Ping Pong")
 
 BLUE = (135, 206, 235)
+
+ball = pygame.image.load("ball.png")
+ball = pygame.transform.scale(ball, (30, 30))
+
+ball_x = WIDTH // 2 - 15
+ball_y = HEIGHT // 2 - 15
 
 running = True
 
@@ -19,7 +25,9 @@ while running:
             running = False
 
     screen.fill(BLUE)
-    pygame.display.flip()
+    screen.blit(ball, (ball_x, ball_y))
+
+    pygame.display.update()
 
 pygame.quit()
 sys.exit()
